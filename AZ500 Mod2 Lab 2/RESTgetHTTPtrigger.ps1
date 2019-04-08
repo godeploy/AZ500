20e02ac0a68eb6ada9fe6e89daaee527ec296770
kramit <mike@michaelwhitehouse.com>
2019-04-08 16:40:39 +0100

    Create RESTgetHTTPtrigger.ps1

---
+++ AZ500 Mod2 Lab 2/RESTgetHTTPtrigger.ps1
@@ -0,0 +1,15 @@
+﻿# Replace this with yours ;)
+$functionappname = ""
+$functionname = "HttpTriggerPowerShell1"
+$functionkey = ""
+
+$YourURI = "https://$functionappname.azurewebsites.net/api/$functionname`?code=$functionkey"
+
+# Try a normal GET
+Invoke-RestMethod -Method Get -Uri $YourURI
+# GET using the "name" query parameter
+Invoke-RestMethod -Method Get -Uri "$($YourURI)&name=World!"
+
+# Use the POST method provided
+$Body = @{name = 'Max Power'} | ConvertTo-Json
+Invoke-RestMethod -Method Post -Body $Body -Uri $YourURI
