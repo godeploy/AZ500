bdc4be288f2f7cffc8fb643fcea47308d3989bef
kramit <mike@michaelwhitehouse.com>
2019-04-08 16:45:12 +0100

    Update RESTgetHTTPtrigger.ps1

--- AZ500 Mod2 Lab 2/RESTgetHTTPtrigger.ps1
+++ AZ500 Mod2 Lab 2/RESTgetHTTPtrigger.ps1
@@ -6,9 +6,17 @@ $functionkey = ""
 $YourURI = "https://$functionappname.azurewebsites.net/api/$functionname`?code=$functionkey"
 
 # Try a normal GET
+
+Write-host " This is result of  a normal GET operation calling your HTTP trigger" -ForegroundColor Yellow
+
 Invoke-RestMethod -Method Get -Uri $YourURI
+
+Write-host " This is result of  a normal GET operation calling your HTTP trigger with an extra parameter passed to the trigger" -ForegroundColor Yellow
 # GET using the "name" query parameter
 Invoke-RestMethod -Method Get -Uri "$($YourURI)&name=World!"
+
+
+Write-host " This is result of a normal PUT operation calling your HTTP trigger that feeds a hash table converted to JSON to the HTTP triggger" -ForegroundColor Yellow
 
 # Use the POST method provided
 $Body = @{name = 'Max Power'} | ConvertTo-Json
