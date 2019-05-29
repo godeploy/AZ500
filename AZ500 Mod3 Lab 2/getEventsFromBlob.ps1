771ccbe644484567f9d348c661013e5dead42569
kramit <mike@michaelwhitehouse.com>
2019-05-29 23:28:17 +0100

    Update getEventsFromBlob.ps1

--- AZ500 Mod3 Lab 2/getEventsFromBlob.ps1
+++ AZ500 Mod3 Lab 2/getEventsFromBlob.ps1
@@ -10,9 +10,8 @@ $containerName =""
 # Load the System.Web assembly to enable UrlEncode
 
 
-$BlobURL = "https://$blobname.blob.core.windows.net/$containerName?restype=container&comp=list&include=metadata"
-$bloblist = Invoke-RestMethod -Method Get -Uri $BlobURL -ContentType "application/Xml" -ErrorAction SilentlyContinue
+$BlobURL = "https://$blobname.blob.core.windows.net/$containerName`?restype=container&comp=list&include=metadata"
+$bloblist = Invoke-RestMethod -Method Get -Uri $BlobURL -ContentType "application/xml" -ErrorAction SilentlyContinue
 [xml]$bloblist = $bloblist.Substring($bloblist.IndexOf("<"))
 
 $bloblist.EnumerationResults.Blobs.Blob | select name, url, @{n = "Timestamp"; e= {$bloblist.EnumerationResults.Blobs.Blob.Properties.'Last-Modified'[0]}} | ft -AutoSize
-
